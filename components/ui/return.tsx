862783d9cfda09f0010bc462dfaa67a254a3cd41
Xvois <sonnymparker9@gmail.com>
2024-05-01 20:28:55 +0100

    set excluded paths for return.tsx

--- components/ui/return.tsx
+++ components/ui/return.tsx
@@ -7,10 +7,11 @@ import {Button} from "@/components/ui/button";
 
 
 export const Return = (): ReactElement<"button"> => {
+    const excludedPaths = ["/login", "/signup", "/"];
     const router = useRouter()
     const path = usePathname();
 
-    if (path === "/") return <></>;
+    if (excludedPaths.includes(path)) return <></>;
 
     return (
         <Button variant={"link"} className={"group inline-flex gap-1 w-fit pl-0"} onClick={() => router.back()}>
