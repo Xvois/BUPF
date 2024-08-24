9c69e75c1cc9a51045779a6f19cf63388d6832ab
Sonny <sonnymparker9@gmail.com>
2024-08-24 13:55:52 +0100

    Separate production and dev URLS for apiAxios

--- utils/axios/apiAxios.tsx
+++ utils/axios/apiAxios.tsx
@@ -14,91 +14,99 @@ import {TopicsResponse} from "@/types/api/topics/types";
 
 
 export type GetRouteResponseMap = {
-	'/api/courses': CoursesResponse,
-	'/api/courses/[id]': CourseResponse,
-	'/api/courses/[id]/modules': CourseModulesResponse,
-	'/api/modules': ModulesResponse,
-	'/api/modules/[id]': ModuleResponse,
-	'/api/posts/[id]': PostResponse,
-	'/api/posts': PostsResponse,
-	'/api/profiles/[id]': ProfileResponse,
-	'/api/user/modules': UserModulesResponse,
-	'/api/articles': ArticlesResponse,
-	'/api/drafts': DraftsResponse,
-	'/api/topics': TopicsResponse
+    '/api/courses': CoursesResponse,
+    '/api/courses/[id]': CourseResponse,
+    '/api/courses/[id]/modules': CourseModulesResponse,
+    '/api/modules': ModulesResponse,
+    '/api/modules/[id]': ModuleResponse,
+    '/api/posts/[id]': PostResponse,
+    '/api/posts': PostsResponse,
+    '/api/profiles/[id]': ProfileResponse,
+    '/api/user/modules': UserModulesResponse,
+    '/api/articles': ArticlesResponse,
+    '/api/drafts': DraftsResponse,
+    '/api/topics': TopicsResponse
 };
 
 export type Params = {
-	searchParams?: string;
-	[key: string]: string | undefined;
+    searchParams?: string;
+    [key: string]: string | undefined;
 }
 
 class APIAxios {
-	private axiosInstance: typeof Axios.prototype;
-	constructor(config?: AxiosRequestConfig) {
-		this.axiosInstance = axios.create(config);
+    private axiosInstance: typeof Axios.prototype;
 
-	}
+    constructor(config?: AxiosRequestConfig) {
+        this.axiosInstance = axios.create(config);
 
-	/**
-	 * This method is used to send a GET request to a specified route and return a typed response.
-	 * The route is specified as a key of the RouteResponseMap type, and the ids parameter is used to replace placeholders in the route.
-	 *
-	 * @template Route - The route type, which should be a key of GetRouteResponseMap.
-	 * @param {Route} route - The route to which the GET request should be sent.
-	 * @param {Params} params - An object where the keys are the parameter names and the values are the parameter
-	 * values, which will be used to replace placeholders in the route and append search parameters to the URL.
-	 * @param {AxiosRequestConfig} [config] - Optional configuration for the axios request.
-	 * @returns {Promise<AxiosResponse<GetRouteResponseMap[Route]>>} - A Promise that resolves to the response of the GET request.
-	 *
-	 * @example
-	 * // Send a GET request to the '/api/courses/[id]' route, replacing '[id]' with '33'
-	 * apiAxios.get('/api/courses/[id]', { id: '33' })
-	 *   .then(response => {
-	 *     // Handle the response
-	 *     console.log(response.data);
-	 *   })
-	 *   .catch(error => {
-	 *     // Handle the error
-	 *     console.error(error);
-	 *   });
-	 */
-	async get<Route extends keyof GetRouteResponseMap>(
-		route: Route,
-		params?: Params,
-		config?: AxiosRequestConfig
-	): Promise<AxiosResponse<GetRouteResponseMap[Route]>> {
+    }
 
-		const isRelativeUrl = (url: string) => {return !(/^(?:[a-z]+:)?\/\//i.test(url));}
+    /**
+     * This method is used to send a GET request to a specified route and return a typed response.
+     * The route is specified as a key of the RouteResponseMap type, and the ids parameter is used to replace placeholders in the route.
+     *
+     * @template Route - The route type, which should be a key of GetRouteResponseMap.
+     * @param {Route} route - The route to which the GET request should be sent.
+     * @param {Params} params - An object where the keys are the parameter names and the values are the parameter
+     * values, which will be used to replace placeholders in the route and append search parameters to the URL.
+     * @param {AxiosRequestConfig} [config] - Optional configuration for the axios request.
+     * @returns {Promise<AxiosResponse<GetRouteResponseMap[Route]>>} - A Promise that resolves to the response of the GET request.
+     *
+     * @example
+     * // Send a GET request to the '/api/courses/[id]' route, replacing '[id]' with '33'
+     * apiAxios.get('/api/courses/[id]', { id: '33' })
+     *   .then(response => {
+     *     // Handle the response
+     *     console.log(response.data);
+     *   })
+     *   .catch(error => {
+     *     // Handle the error
+     *     console.error(error);
+     *   });
+     */
+    async get<Route extends keyof GetRouteResponseMap>(
+        route: Route,
+        params?: Params,
+        config?: AxiosRequestConfig
+    ): Promise<AxiosResponse<GetRouteResponseMap[Route]>> {
 
-		if (!isRelativeUrl(route as string)) {
-			throw new Error('(apiAxios): Route must be a relative URL.');
-		}
+        const isRelativeUrl = (url: string) => {
+            return !(/^(?:[a-z]+:)?\/\//i.test(url));
+        }
 
-		let defaultURL: string;
+        if (!isRelativeUrl(route as string)) {
+            throw new Error('(apiAxios): Route must be a relative URL.');
+        }
 
-		if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-			defaultURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-		} else if (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) {
-			defaultURL = `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
-		}   else {
-			defaultURL = 'http://localhost:3000';
-		}
+        let defaultURL: string;
 
-		// Replace all instances of something enclosed in square brackets with its corresponding string in the ids parameter
-		let url = `${defaultURL}${route}`;
-		for (const key in params) {
-			if (params[key] !== undefined && key !== 'searchParams') {
-				url = url.replace(`[${key}]`, params[key] as string);
-			}
-		}
-		// Append the search params to the URL
-		if (params && params.searchParams) {
-			url = url + `?${params.searchParams}`;
-		}
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'development') {
+            if (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL) {
+                defaultURL = `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
+            } else if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+                defaultURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+            } else {
+                defaultURL = 'http://localhost:3000';
+            }
+        } else {
+            defaultURL = `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+        }
 
-		return this.axiosInstance.get(url, config);
-	}
+
+        // Replace all instances of something enclosed in square brackets with its corresponding string in the ids parameter
+        let url = `${defaultURL}${route}`;
+        for (const key in params) {
+            if (params[key] !== undefined && key !== 'searchParams') {
+                url = url.replace(`[${key}]`, params[key] as string);
+            }
+        }
+        // Append the search params to the URL
+        if (params && params.searchParams) {
+            url = url + `?${params.searchParams}`;
+        }
+
+        return this.axiosInstance.get(url, config);
+    }
 }
 
 const apiAxios: APIAxios = new APIAxios();
