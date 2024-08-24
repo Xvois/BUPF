e1f0d9613baaae4472204ab49f55568972635d03
Sonny <sonnymparker9@gmail.com>
2024-08-24 13:36:19 +0100

    Fix CodeQL incomplete string escaping error

--- utils/api/helpers.ts
+++ utils/api/helpers.ts
@@ -107,12 +107,12 @@ export const wrapQParams = (filters: Filter[] = [], sort?: Sort, limit?: number)
 export function toPostgresList(value: string | string[]): string {
     if (Array.isArray(value)) {
         let str = '(';
-        str += value.map(item => `"${item.replace(/"/g, '\\"')}"`).join(',');
+        str += value.map(item => `"${item.replace(/\\/g, '\\\\').replace(/"/g, '\\"')}"`).join(',');
         str += ')';
         return str;
     } else {
         // Handle the case where value is a string
-        return `"${value.replace(/"/g, '\\"')}"`;
+        return `"${value.replace(/\\/g, '\\\\').replace(/"/g, '\\"')}"`;
     }
 }
 
